bf60afa3e836c821f4d50e29f7433cfa90806558
Oscar Shoudt <shoudto@gmail.com>
2019-08-04 20:59:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,8 +18,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_hash[:fourth_of_july]
-  binding.pry 
+  holiday_hash[:summer][:fourth_of_july][1]  
+  # binding.pry 
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
