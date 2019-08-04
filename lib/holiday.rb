7d6d77b4d6e70cedca0ce79a56e34e26598dd54b
Oscar Shoudt <shoudto@gmail.com>
2019-08-04 21:02:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter][:christmas] << supply
   holiday_hash[:winter][:new_years] << supply 
-  binding.pry
 end
 
 
