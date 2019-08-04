ad4eef8654204fcbd96e1722e477734ce0f0e36c
Oscar Shoudt <shoudto@gmail.com>
2019-08-04 21:53:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
-    binding.pry 
+    holiday.map do 
+
+    end
   end
 end
