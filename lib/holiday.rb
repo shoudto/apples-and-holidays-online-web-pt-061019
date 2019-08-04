e06ffb7a4f046cdf8dab4ede83ff093e279eef2a
Oscar Shoudt <shoudto@gmail.com>
2019-08-04 21:56:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, item|
-      holiday if item.include?("BBQ") 
+      holiday if item.include?("BBQ")
       end
-    end
+    end.flatten.compact
   end
